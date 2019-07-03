c8736acaa7934cb66bb91e26beef2a52954a845d
Stéphane Dreyfus <floatingrock@gmail.com>
2019-07-02 21:19:03 -0700

    Starting to add dice rules.

---
+++ src/Rules.js
@@ -0,0 +1,20 @@
+/** Rules for scoring Giants and Halflings
+ * 
+ * This abstract class contains the rules as subclasses.
+ * All passed in parameters are stored as properties on the instance.
+ * 
+ * This contains the functions that will calculate losses, and
+ * payouts for win conditions.
+ */
+
+class Rule {
+  constructor(params) {
+    Object.assign(this, params);
+  }
+
+  /** Sum of Halfling dice
+   * If split: true, sum of a set of Halfling dice */
+  sum(dice) {
+    return dice.reduce((prev, curr) => prev + curr);
+  }
+}
