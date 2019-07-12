51de89774bc85de2222c8b51f491851cce0fcad2
Stéphane Dreyfus <floatingrock@gmail.com>
2019-07-12 15:55:55 -0700

    Rules work, committing before refactor.

--- src/Rules.js
+++ src/Rules.js
@@ -4,6 +4,21 @@
  * @param {int} giantDie
  */
 
+class Rules {
+  constructor(params) {
+    Object.assign(this, params);
+  }
 
+  sum(halflingDice) {
+    return halflingDice.reduce((prev, curr)=> prev + curr);
+  }
+
+  evalResults(halflingSum, giantDie, pot, ante) {
+    if (giantDie === 1) return 
+  }
+
+}
 
 export default Rules;
+
+// return boolean and have component set state depending on boolena value
